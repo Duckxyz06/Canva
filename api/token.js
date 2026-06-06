b25c4492893c6b599c7b26be8d12f9d71648fd5f
Duckxyz06 <nguyenvietduc06xyz@gmail.com>
2026-06-06 11:10:30 +0700

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,6 +1,21 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    hasClientId: !!process.env.CANVA_CLIENT_ID,
-    hasSecret: !!process.env.CANVA_CLIENT_SECRET
-  });
+export default async function handler(req, res) {
+  try {
+    const { code } = req.query;
+
+    if (!code) {
+      return res.status(400).json({
+        error: "Missing code"
+      });
+    }
+
+    return res.status(200).json({
+      receivedCode: true,
+      codeLength: code.length
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message
+    });
+  }
 }
